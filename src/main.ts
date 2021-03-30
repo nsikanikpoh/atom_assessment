f881cb485d5d7550627f79d91dbd636397c76a23
Nsikan Ikpoh <nsikanikpoh@gmail.com>
2021-03-30 16:54:17 +0100

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ async function bootstrap() {
   const whitelist = ['https://webservice.com', 'https://www.webservice.com'];
   app.enableCors({
   origin: function (origin, callback) {
-    // if (whitelist.indexOf(origin) !== -1) {
-    //   console.log("allowed cors for:", origin)
-    //   callback(null, true)
-    // } else {
-    //   console.log("blocked cors for:", origin)
-    //   callback(new Error('Not allowed by CORS'))
-    // }
+    if (whitelist.indexOf(origin) !== -1) {
+      console.log("allowed cors for:", origin)
+      callback(null, true)
+    } else {
+      console.log("blocked cors for:", origin)
+      callback(new Error('Not allowed by CORS'))
+    }
     callback(null, true)
   },
   allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
